d96547c5597ad95b7e73b46cf5aa27010c0f3149
Carolyn Barbour <carolynbarbour@hotmail.com>
2023-01-13 16:36:38 +0000

    58. Stateful vs Stateless Widget update

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  static int sides = 6;
+  int leftDiceNumber = Random().nextInt(sides) + 1;
+  int rightDiceNumber = Random().nextInt(sides) + 1;
+
+  int randomDiceNumber() {
+    return Random().nextInt(sides) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var randDiceOne = Random().nextInt(5) + 1;
-    var randDiceTwo = Random().nextInt(5) + 1;
-
     return SafeArea(
       child: Center(
         child: Row(
           children: [
             Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice$randDiceOne.png'),
-            )),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        leftDiceNumber = randomDiceNumber();
+                      });
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png'))),
             Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice$randDiceTwo.png'),
+                child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = randomDiceNumber();
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             )),
           ],
         ),
